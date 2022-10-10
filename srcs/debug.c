5f2555279fbe919a781eae5a88d68c6516fdeaf7
Hevhove <hjg.van.hove@gmail.com>
2022-10-10 19:43:46 +0100

    removed useless transparancy values

--- srcs/debug.c
+++ srcs/debug.c
@@ -8,10 +8,10 @@ void	print_cub(t_cub *cub)
 	printf("\n***	🎨 CUB 🍻	***\n\n");
 	// printf("NO fd: %d, SO fd: %d, WE fd: %d, EA fd: %d\n",
 	// 	cub->walls.no_fd, cub->walls.so_fd, cub->walls.we_fd, cub->walls.ea_fd);
-	printf("Floor color: #%X%X%X%X or rgb(%d, %d, %d)\n", get_t(cub->floor_color),
+	printf("Floor color: #%X%X%X or rgb(%d, %d, %d)\n",
 		get_r(cub->floor_color), get_g(cub->floor_color), get_b(cub->floor_color),
 		get_r(cub->floor_color), get_g(cub->floor_color), get_b(cub->floor_color));
-	printf("Ceiling color: #%X%X%X%X or rgb(%d, %d, %d)\n", get_t(cub->ceil_color),
+	printf("Ceiling color: #%X%X%X or rgb(%d, %d, %d)\n",
 		get_r(cub->ceil_color), get_g(cub->ceil_color), get_b(cub->ceil_color),
 		get_r(cub->ceil_color), get_g(cub->ceil_color), get_b(cub->ceil_color));
 	printf("\n🗺  Map height %d x width %d\n", cub->map_height, cub->map_width);
