d25a1c20cd47ff378ce6d3bad00c564d938998aa
Sara Vieira <s.cvieira@sapo.pt>
2022-10-12 14:15:38 +0200

    using img width and height

--- srcs/initializers/init_mlx.c
+++ srcs/initializers/init_mlx.c
@@ -25,28 +25,27 @@ static void	init_textures(t_cub *cub)
  * images: main img, walls' images and map/navigator image
 * +2 when sizing navigator: accomodating for borders
 */
-// TODO Use width and height from image struct ? meh
 void	init_mlx(t_cub *cub)
 {
-	int	nav_width;
-	int	nav_height;
-
 	cub->mlx = mlx_init();
 	cub->window = mlx_new_window(cub->mlx, WIDTH, HEIGHT, "Let's play!");
 	cub->img = (t_img *)calloc_or_exit(sizeof(t_img), 1, cub);
+	cub->img->width = WIDTH;
+	cub->img->height = HEIGHT;
 	cub->img->img_ptr = mlx_new_image(cub->mlx, WIDTH, HEIGHT);
 	cub->img->addr = mlx_get_data_addr(cub->img->img_ptr,
 			&cub->img->bits_per_pixel, &cub->img->line_length,
 			&cub->img->endian);
 	init_textures(cub);
 	cub->nav_img = (t_img *)calloc_or_exit(sizeof(t_img), 1, cub);
-	nav_width = PIXEL_PER_SQUARE * (cub->map_width + 2);
-	if (nav_width > WIDTH / NAV_FRACT + 2 * PIXEL_PER_SQUARE)
-		nav_width = WIDTH / NAV_FRACT + 2 * PIXEL_PER_SQUARE;
-	nav_height = PIXEL_PER_SQUARE * (cub->map_height + 2);
-	if (nav_height > HEIGHT / NAV_FRACT + 2 * PIXEL_PER_SQUARE)
-		nav_height = HEIGHT / NAV_FRACT + 2 * PIXEL_PER_SQUARE;
-	cub->nav_img->img_ptr = mlx_new_image(cub->mlx, nav_width, nav_height);
+	cub->nav_img->width = PIXEL_PER_SQUARE * (cub->map_width + 2);
+	if (cub->nav_img->width > WIDTH / NAV_FRACT + 2 * PIXEL_PER_SQUARE)
+		cub->nav_img->width = WIDTH / NAV_FRACT + 2 * PIXEL_PER_SQUARE;
+	cub->nav_img->height = PIXEL_PER_SQUARE * (cub->map_height + 2);
+	if (cub->nav_img->height > HEIGHT / NAV_FRACT + 2 * PIXEL_PER_SQUARE)
+		cub->nav_img->height = HEIGHT / NAV_FRACT + 2 * PIXEL_PER_SQUARE;
+	cub->nav_img->img_ptr = mlx_new_image(cub->mlx, cub->nav_img->width,
+			cub->nav_img->height);
 	cub->nav_img->addr = mlx_get_data_addr(cub->nav_img->img_ptr,
 			&cub->nav_img->bits_per_pixel, &cub->nav_img->line_length,
 			&cub->nav_img->endian);
