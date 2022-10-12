06bc31944e869ee25491fd9c7fc37c394c8dfb95
Hevhove <hjg.van.hove@gmail.com>
2022-10-12 17:52:37 +0100

    feat: mouse movement added

--- srcs/render/render.c
+++ srcs/render/render.c
@@ -9,18 +9,15 @@ void	render(t_cub *cub)
 /*
 	UPDATE_DISPLAY:
 	-	Inside of mlx_loop_hook, so happening when NO event happens
-	-	Checks the key value in the relevant struct set by key_down and key_up
+	-	Checks the key value in the relevant struct set by key_down, key_up and
+		mouse_hook
 	-	If values in the struct are non-zero, apply movement to coords
 
-	-	VAR: Edge detection = 1.1]
-	-	VAR: Movement Speed = 0.05 squares/frame]
-	-	VAR: Rotation Speed = 0.02 radians]
+	-	VAR: Edge detection = checks if map exists 10% beyond next square (1.1)
+	-	VAR: Movement Speed = squares/frame (MOVE_SPEED macro)
+	-	VAR: Rotation Speed = radians/frame (ROT_SPEED macro)
 
-	-	Above values were set by experimentation. We can also change the
-		static int frame value in the if statement
-
-	-	Frametime: need to establish some values here that make the project
-		run smoothly accross both Macs and Linux. Currently in milliseconds
+	-	Above values were set by experimentation for both MacOS and Linux.
 */
 int	update_display(t_cub *cub)
 {
@@ -32,13 +29,17 @@ int	update_display(t_cub *cub)
 		rotate_left(cub, ROT_SPEED);
 	else if (cub->keys.right)
 		rotate_right(cub, ROT_SPEED);
+	if (cub->mouse.left)
+		rotate_left(cub, ROT_SPEED * cub->mouse.left / WIDTH);
+	else if (cub->mouse.right)
+		rotate_right(cub, ROT_SPEED * cub->mouse.right / WIDTH);
 	if (cub->keys.a)
 		move_left(cub, 1.1, MOVE_SPEED);
 	else if (cub->keys.d)
 		move_right(cub, 1.1, MOVE_SPEED);
 	if (cub->keys.w || cub->keys.a || cub->keys.s || cub->keys.d
 		|| cub->keys.up || cub->keys.down || cub->keys.left
-		|| cub->keys.right)
+		|| cub->keys.right || cub->mouse.left || cub->mouse.right)
 		render(cub);
 	return (0);
 }
