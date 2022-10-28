0a59cac4258cf8bfe2856f725f1dba3df81448bd
Sara Vieira <s.cvieira@sapo.pt>
2022-10-28 11:50:26 +0100

    removing an extra =

--- srcs/render/textures.c
+++ srcs/render/textures.c
@@ -89,7 +89,7 @@ void	draw_texture(int x, int i, t_cub *cub)
 
 	if (i < cub->draw_start)
 		color = cub->c_color;
-	else if (i >= cub->draw_start && i <= cub->draw_end)
+	else if (i >= cub->draw_start && i < cub->draw_end)
 	{
 		cub->texel->y = (int)cub->texel_pos;
 		if (cub->texel->y < 0)
